a263f8d88349cdc9624c0046c4a067b021091db8
Agustin Z <86991224+azakalik@users.noreply.github.com>
2024-09-21 22:08:00 -0300

    Added React Router v6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,24 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import LandingPage from "./pages/LandingPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/test",
+    element: <div>Test</div>,
+  }
+]);
+
 export default function App() {
   return <MantineProvider theme={theme}>
-    <LandingPage />
+    <RouterProvider router={router} />
   </MantineProvider>;
 }

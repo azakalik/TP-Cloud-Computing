0ef41d1ae5be0e27a78466c4a39ed41b5fa9b558
martinippo01 <martinippo@gmail.com>
2024-09-24 00:01:53 -0300

    fix: modify fields so it can be recieved as zaka wanrs

--- backend/lambdas/publications/postPublications/index.js
+++ backend/lambdas/publications/postPublications/index.js
@@ -6,20 +6,36 @@ const { v4: uuidv4 } = require("uuid");
 const dynamoDB = new DynamoDBClient({ region: "us-east-1" });
 const s3 = new S3Client({ region: "us-east-1" });
 
+
+function getExtensionFromBase64(base64String) {
+    // Extract the part before "base64," which contains the MIME type
+    const mimeType = base64String.match(/^data:(.+);base64,/);
+    
+    // If found, split the MIME type and return the extension (e.g., "jpeg", "png")
+    if (mimeType) {
+        return mimeType[1].split('/')[1]; // Get the part after "image/"
+    }
+
+    return null; // Return null if the format is not correct
+}
+
+
 exports.handler = async (event) => {
     
     let publicationId, createdTime, dueTimeISO, item1, item2, item3, imageUrl;
     
     try {
         // Parse incoming JSON (containing image as base64, filename, and other data)
-        const { user, initialPrice, dueTime, title, description, image, filename, country = "AR" } = JSON.parse(event.body); // Default country to "AR" if not provided
+        const { user, initialPrice, endTime, title, description, images, countryFlag = "AR" } = JSON.parse(event.body); // Default country to "AR" if not provided
     
         // Generate unique publication ID and timestamps
         publicationId = `PUBID#${uuidv4()}`;
         createdTime = new Date().toISOString();
-        dueTimeISO = new Date(dueTime).toISOString();
+        dueTimeISO = new Date(endTime).toISOString();
         
-        const base64Image = image.replace(/^data:image\/\w+;base64,/, "");
+        const filename = publicationId.replace("PUBID#", "") + "_" + "0" + "." + getExtensionFromBase64(images[0])
+        
+        const base64Image = images[0].replace(/^data:image\/\w+;base64,/, "");
         const buffer = Buffer.from(base64Image, "base64");
     
         // S3 upload parameters with public-read ACL for access
@@ -52,7 +68,7 @@ exports.handler = async (event) => {
                 Description: { S: description },
                 Created: { S: createdTime },
                 Image: { S: imageUrl },
-                Country: { S: country } // Add country field
+                Country: { S: countryFlag } // Add country field
             }
         };
     } catch (error) {

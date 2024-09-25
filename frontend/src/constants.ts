7448dfaa6134da047486f87b8615881f160e6170
Agustin Z <86991224+azakalik@users.noreply.github.com>
2024-09-25 02:36:22 -0300

    Update constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,15 +2,13 @@ let stage = 'prod';  // Change this to 'prod' for production
 
 let HIGHEST_BID_WS_URL: string;
 let API_GW_URL: string;
-let API_GW_BIDS_URL: string;
 
 if (stage === 'prod') {
    HIGHEST_BID_WS_URL = '';  // Set the production URL here
    API_GW_URL = '';  // Set the production URL here
-   API_GW_BIDS_URL = ''
 } else if (stage === 'dev') {
    HIGHEST_BID_WS_URL = 'ws://localhost:8000';
-   API_GW_URL = ''; // Set if needed
+   API_GW_URL = 'http://localhost:3001';
 }
 
 export { HIGHEST_BID_WS_URL, API_GW_URL };

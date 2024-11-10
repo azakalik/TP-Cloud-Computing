bf3032153c4bd556258067abebe11d6260ce6ea5
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-11-10 13:45:33 -0300

    fix: sql init file

--- backend/lambdas/offers/functions/initDb/init.sql
+++ backend/lambdas/offers/functions/initDb/init.sql
@@ -1,3 +1,14 @@
+CREATE TABLE IF NOT EXISTS balance (
+    user_id VARCHAR PRIMARY KEY,
+    total NUMERIC NOT NULL,
+    email VARCHAR NOT NULL,
+    available NUMERIC NOT NULL,
+    CONSTRAINT balance_total_available_check CHECK (total >= available),
+    CONSTRAINT balance_email_unique UNIQUE (email),
+    CONSTRAINT balance_total_positive_check CHECK (total >= 0),
+    CONSTRAINT balance_available_positive_check CHECK (available >= 0)
+);
+
 CREATE TABLE IF NOT EXISTS offers (
     offer_id UUID PRIMARY KEY,
     publication_id VARCHAR NOT NULL,
@@ -8,17 +19,6 @@ CREATE TABLE IF NOT EXISTS offers (
     CONSTRAINT offers_pub_price_unique UNIQUE (publication_id, price),
     CONSTRAINT offers_user_id_fk FOREIGN KEY (user_id) REFERENCES balance (user_id),
     CONSTRAINT offers_price_positive_check CHECK (price > 0)
-);
-
-CREATE TABLE IF NOT EXISTS balance (
-    user_id VARCHAR PRIMARY KEY,
-    total NUMERIC NOT NULL,
-    email VARCHAR NOT NULL,
-    available NUMERIC NOT NULL,
-    CONSTRAINT balance_total_available_check CHECK (total >= available),
-    CONSTRAINT balance_email_unique UNIQUE (email),
-    CONSTRAINT balance_total_positive_check CHECK (total >= 0),
-    CONSTRAINT balance_available_positive_check CHECK (available >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS closed_auctions (
